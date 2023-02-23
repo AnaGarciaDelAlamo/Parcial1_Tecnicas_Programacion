ee0e1e197580d48426e0de0ec5cfd8ed4426946a
ana <andelamo@myuax.com>
2023-02-23 19:49:06 +0100

    PRACTICA2CLASESCREADAS

--- src/main/java/Practica2/Tablero.java
+++ src/main/java/Practica2/Tablero.java
@@ -2,9 +2,26 @@ package Practica2;
 
 public class Tablero {
     private static int DIMENSION = 30;
-    private int [][] estadoActual;
+    private int[][] estadoActual;
 
-    private int [][] estadoSiguiente = new int [DIMENSION][DIMENSION];
+    private int[][] estadoSiguiente = new int[DIMENSION][DIMENSION];
+
+    public void leerEstadoActual() {
 
 
+    }
+
+    public void generarEstadoActualPorMontecarlo(){
+
+    }
+
+    public void transitarAlEstadoSiguiente(){
+
+    }
+
+    public String toString(){
+        return "El estado actual es: " + estadoActual;
+    }
 }
+
+
