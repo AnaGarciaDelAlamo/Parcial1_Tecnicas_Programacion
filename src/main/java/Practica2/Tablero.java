1c7b870b8199fc9e2f7c8e19e5892ba0bf8afca7
ana <andelamo@myuax.com>
2023-02-25 13:32:29 +0100

    continuacion practica2

--- src/main/java/Practica2/Tablero.java
+++ src/main/java/Practica2/Tablero.java
@@ -6,20 +6,20 @@ public class Tablero {
 
     private int[][] estadoSiguiente = new int[DIMENSION][DIMENSION];
 
-
     public void leerEstadoActual() {
-        estadoActual = new int[DIMENSION][DIMENSION];
         for (int i = 0; i < DIMENSION; i++) {
             for (int j = 0; j < DIMENSION; j++) {
-                System.out.println(estadoActual[i][j] = (Math.random() < 0.5) ? 1 : 0);
+                estadoActual[i][j] = (int) (Math.random() * 2);
+                System.out.println(estadoActual[i][j]);
             }
         }
+        System.out.println();
 
 
     }
 
     public void generarEstadoActualPorMontecarlo() {
-        estadoActual = new int[DIMENSION][DIMENSION];
+        int[][] estadoActual = new int[DIMENSION][DIMENSION];
         for (int i = 0; i < DIMENSION; i++) {
             for (int j = 0; j < DIMENSION; j++) {
                 estadoActual[i][j] = (Math.random() < 0.5) ? 1 : 0;
@@ -28,13 +28,34 @@ public class Tablero {
     }
 
     public void transitarAlEstadoSiguiente() {
-        estadoSiguiente = new int[DIMENSION][DIMENSION];
         for (int i = 0; i < DIMENSION; i++) {
             for (int j = 0; j < DIMENSION; j++) {
-                estadoSiguiente[i][j] = (Math.random() < 0.5) ? 1 : 0;
+                int vecinosVivos = contarVecinosVivos(i, j);
+                if (vecinosVivos == 3) {
+                    estadoSiguiente[i][j] = 1;
+                } else if (vecinosVivos == 2) {
+                    estadoSiguiente[i][j] = estadoActual[i][j];
+                } else {
+                    estadoSiguiente[i][j] = 0;
+                }
             }
         }
         estadoActual = estadoSiguiente;
+    }
+
+    private int contarVecinosVivos(int i, int j) {
+        int vecinosVivos = 0;
+        for (int k = i - 1; k <= i + 1; k++) {
+            for (int l = j - 1; l <= j + 1; l++) {
+                if (k >= 0 && k < DIMENSION && l >= 0 && l < DIMENSION && estadoActual[k][l] == 1) {
+                    vecinosVivos++;
+                }
+            }
+        }
+        if (estadoActual[i][j] == 1) {
+            vecinosVivos--;
+        }
+        return vecinosVivos;
     }
 
     public String toString() {
